27f221f718067277891e266459b826c7394dfc4d
sarahpst <111440718+sarahpst@users.noreply.github.com>
2023-05-10 03:08:07 +0200

    done without styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+
+  console.log(firstFive);
+
+  const addContact = () => {
+    let randomContactIndex = Math.floor(
+      Math.random() * (contacts.length - 5) + 5
+    );
+    const newContact = contacts[randomContactIndex];
+    setFirstFive([...firstFive, newContact]);
+  };
+
+  const sortByName = () => {
+    const orderedByName = [...firstFive].sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    setFirstFive(orderedByName);
+  };
+
+  const sortByPopularity = () => {
+    const orderedByPopularity = [...firstFive].sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setFirstFive(orderedByPopularity);
+  };
+
+  const deleteContact = (contactId) => {
+    const filteredContact = firstFive.filter((contact) => {
+      return contact.id !== contactId;
+    });
+
+    setFirstFive(filteredContact);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+      <h1>Iron contacts</h1>``
+      <button onClick={() => addContact()}>add a new contact</button>
+      <button onClick={() => sortByName()}>Sort by Name</button>
+      <button onClick={() => sortByPopularity()}>Sort by Popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy </th>
+            <th>Actions</th>
+          </tr>
+        </thead>
 
-export default App
+        <tbody>
+          {firstFive.map((contact) => (
+            <tr>
+              <th>
+                <img
+                  style={{ width: 100 }}
+                  src={contact.pictureUrl}
+                  alt="profilepic"
+                />
+              </th>
+              <th>{contact.name}</th>
+              <th>{contact.popularity}</th>
+              <th>{contact.wonOscar && "🏆"}</th>
+              <th>{contact.wonEmmy && "🏆"}</th>
+              <th>
+                <button onClick={() => deleteContact(contact.id)}>
+                  Delete
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default App;
